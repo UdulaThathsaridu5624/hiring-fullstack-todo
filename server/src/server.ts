ff69144b2c0798ed148a52b2fd6e3d0440d75cfd
IT22056320 <it22056320@my.sliit.lk>
2026-05-05 19:28:29 +0530

    chore(server): connect mongodb with mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import app from "./app";
+import connectDB from "./config/db";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+}).catch((error) => {
+  console.error("Failed to connect to the database", error);
 });
